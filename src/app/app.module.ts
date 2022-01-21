1894d596995db3c1d650a17337906b2f622df583
Dương  Điệp <zenkyxz@gmail.com>
2022-01-21 08:32:27 +0700

    dsdsd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgePipe } from './pipes/age.pipe';
+import { ProductListComponent } from './screens/product-list/product-list.component';
+import { StarComponent } from './component/star/star.component';
+import { FormsModule } from '@angular/forms';
+import { NumberArrayPipe } from './pipes/number-array.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgePipe,
+    ProductListComponent,
+    StarComponent,
+    NumberArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/star/star.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit {
+  @Input() rate: number = 0;
+  @Input() productId: number = 0;
+  @Output() voted = new EventEmitter<any>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  updateStar(vote: any){
+    // console.log(vote, this.productId)
+    this.voted.emit({
+      id: this.productId,
+      star: vote
+    });
+  }
+}
